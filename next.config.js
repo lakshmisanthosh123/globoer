e5ebe83ea3ece00b79660ee1fb43f6e735724b00
lakshmisanthosh123 <lakshmisanthosh1994@gmail.com>
2026-02-27 12:53:10 +0530

    fix

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactCompiler: true,
+  transpilePackages: ["@mui/material", "@mui/icons-material"],
+};
+
+module.exports = nextConfig;
--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactCompiler: true,
-  transpilePackages: ["@mui/material", "@mui/icons-material"],
-};
-
-export default nextConfig;
